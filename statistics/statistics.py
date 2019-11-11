03379168d73be0ab3ec0e0f9a2f4ef53bb6c9bee
zhawhjw <649987907@qq.com>
2019-11-10 21:54:18 -0500

    Feature: update proportion function

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -102,8 +102,46 @@ class Statistics(Calculator):
         self.result = data_sem
         return self.result
 
+    def cdf(self, value: float, mean = 0, stddev = 1):
+        e = math.e
+        pi = math.pi
+
+        subtract = self.subtract
+        divide = self.divide
+        multiply = self.multiply
+        sqr = self.sqr
+        sqrt = self.sqrt
+
+        pow = math.pow
+
+        base = multiply(2,pi)
+        base = sqrt(base)
+        base = multiply(base, stddev)
+        base = divide(base, 1)
+        base = multiply(base, e)
+
+
+        p = subtract(mean, value)
+        p = divide(stddev,p)
+        p = sqr(p)
+        p = multiply(0.5, p)
+        p = -p
+
+        data_cdf = pow(base,p)
+        self.result = data_cdf
+        return self.result
+
+
+
     def mean(self, data: list):
-        #Task 1
+        """
+        Task 1 and 13
+
+        get the mean value of the data
+
+        :param data: data list
+        :return: mean value
+        """
 
         """define function built in class 'Calculator' """
         divide = self.divide
@@ -117,7 +155,16 @@ class Statistics(Calculator):
         return self.result
 
     def median(self, data: list):
-        #Task 2
+        """
+        Task 2
+
+        get the median value of sorted data list;
+        calculation method is depend on Parity of length of the data list
+
+        :param data: data list
+        :return: median value
+        """
+
 
         divide = self.divide
         subtract = self.subtract
@@ -154,27 +201,82 @@ class Statistics(Calculator):
         return self.result
 
     def mode(self, data: list):
-        #Task 3 (NOT FINISH)
+        """
+        Task 3 (NOT FINISH)
+        get the most frequent numbers in data list;
+        will return error when all numbers have same frequency
+        :param data:
+        :return:
+        """
+
         add = self.add
 
         count_set = {}
 
         mode = max(set(data), key=data.count)
 
-    def stddev(self, data: list):
-        #Task 4
+    def stddev(self, data: list, mode="population"):
+        """
+        Task 4 and 14
+
+        Calculate stand deviation for population data or sample data
+
+        :param data: data list
+        :param mode: "population" or "sample"
+        :return: stand deviation
+        """
+
         """returns the standard deviation of data"""
         variance = self.variance
         sqrt = self.sqrt
 
-        data_variance = variance(data)
+        data_variance = variance(data,mode)
         data_stddev= sqrt(data_variance)
 
         self.result = data_stddev
         return self.result
 
+    def vp(self, data:list, success_data_count:int):
+        """
+        Task 5 and 15
+
+        ^p = X / n
+        q = sqrt( ^p * (1 - ^p) / n)
+
+        :param data: data list has size n
+        :param success_data_count: given count of data which matches the characteristics we want X
+        :return: variance of population/sample proportion q
+        """
+        subtract = self.subtract
+        multiply = self.multiply
+        divide = self.divide
+        sqrt = self.sqrt
+
+        proportion = self.proportion
+
+        data_size = len(data)
+
+        data_proportion = proportion(data, success_data_count)
+
+        complementary_proportion = subtract(data_proportion, 1)
+        nominator = multiply(data_proportion, complementary_proportion)
+        denominator = data_size
+        ratio = divide(denominator, nominator)
+        vp = sqrt(ratio)
+
+        self.result = vp
+        return self.result
+
     def zscore(self, value, data: list):
-        #Task 6
+        """
+        Task 6
+        get z-score of data list
+        
+        :param value:
+        :param data:
+        :return:
+        """
+
         divide = self.divide
         subtract = self.subtract
         mean = self.mean
@@ -190,13 +292,29 @@ class Statistics(Calculator):
         return self.result
 
     def standardizedscore(self, value, data:list ):
-        #Task 7
+        """
+        Task 7
+        same meaning with z-score
+
+        :param value:
+        :param data:
+        :return:
+        """
+
         zscore = self.zscore
         self.result = zscore(value,data)
         return self.result
 
     def pcc(self, data1:list, data2:list):
-        #Task 8
+        """
+        Task 8
+        get population correlation coefficient
+
+        :param data1:
+        :param data2:
+        :return:
+        """
+
         divide = self.divide
         multiply = self.multiply
 
@@ -213,9 +331,59 @@ class Statistics(Calculator):
         self.result = data_ppc
         return self.result
 
+    def ci(self, data: list, cl = 0.95):
+        """
+        Task 9
 
-    def variance(self, data: list):
-        #Task 10
+        Confidence interval - given a data list of values compute the stand deviation,
+        divided by the number of entries,
+        multiplied by a constant factor of (cc).
+        The constant factor can be looked up from a table, for 95% confidence
+        on a reasonable size sample (>=500) 1.96 is used.
+        """
+        add=self.add
+        subtract = self.subtract
+        divide = self.divide
+        multiply = self.multiply
+        sqrt = self.sqrt
+
+        mean=self.mean
+        stddev=self.stddev
+
+        data_mean = mean(data)
+        data_size = len(data)
+        data_stddev=stddev(data)
+
+        if cl == 0.95:
+            cc = 1.96
+        elif cl == 0.90:
+            cc = 1.64
+        elif cl == 0.99:
+            cc = 2.58
+        else:
+            cc = 1.96
+            print('Only 0.90, 0.95 or 0.99 % are allowed for, using default 0.95')
+
+        data_size_sqrt = sqrt(data_size)
+        h = divide(data_size_sqrt, data_stddev)
+        h = multiply(h,cc)
+
+        left = subtract(h,data_mean)
+        right = add(h, data_mean)
+
+        self.result = [left, right]
+        return self.result
+
+    def variance(self, data: list, mode="population"):
+        """
+        Task 10
+        get variance of population data or sample data
+
+        :param data:
+        :param mode:
+        :return:
+        """
+
         add = self.add
         subtract = self.subtract
         divide = self.divide
@@ -231,37 +399,45 @@ class Statistics(Calculator):
             diff_sqr = sqr(diff)
             diff_sqr_summation = add(diff_sqr_summation, diff_sqr)
 
-        data_variance = divide(data_size, diff_sqr_summation)
+        if mode == "sample":
+            data_variance = divide(data_size, diff_sqr_summation)
+        else:
+            df = subtract(1, data_size)
+            data_variance = divide(df, diff_sqr_summation)
 
         self.result = data_variance
         return self.result
 
+    def pvalue(self, data:list):
+        """
+        Task 11
+        get p-value
+        :param data:
+        :return:
+        """
+        return
+    def proportion(self, data: list, success_data_count: int):
+        """
+        Task 12
 
+        ^p = X / n
 
+        Where X is the given count of data which matches the characteristics we want
+              n is the sample size where draws the X
 
-    def proportion(self, data: list):
-        #Task 12
-        subtract = self.subtract
+              ^p is the proportion
+
+        :param data: sample size n
+        :param success_data_count: given count of data which matches the characteristics we want X
+        :return: proportion ^p
+        """
+
         divide = self.divide
 
-        sorted_data = sorted(data)
+        data_size = len(data)
 
-        p = []
-
-        data_min = sorted_data[0]
-        data_max = sorted_data[-1]
-
-        total_diff = subtract(data_min, data_max)
-
-        for d in data:
-            proportion_diff = subtract(min, d)
-            data_proportion = divide(total_diff, proportion_diff)
-            p.append(data_proportion)
-
-        self.result = p
+        data_proportion = divide(data_size, success_data_count)
+        self.result = data_proportion
         return self.result
 
-
-
-    #def cdi(self,data:list, cdl = 0.95 ):
 
