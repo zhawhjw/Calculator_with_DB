51b1a1feb5f150ee9930b4ebf74c4de525c9d09e
hao <hao@pop-os.localdomain>
2019-12-08 01:41:54 -0500

    Feature: Update Print() for all queries

--- database-sql/sql-database.py
+++ database-sql/sql-database.py
@@ -200,56 +200,239 @@ q = session.query(Customer)
 for c in q:
     print(c.id, c.first_name)
 
-session.query(Customer.id, Customer.first_name).all()
+print()
 
-session.query(Customer).count()  # get the total number of records in the customers table
-session.query(Item).count()  # get the total number of records in the items table
-session.query(Order).count()  # get the total number of records in the orders table
+print(session.query(Customer.id, Customer.first_name).all())
 
-session.query(Customer).first()
-session.query(Item).first()
-session.query(Order).first()
+print (session.query(Customer).count())  # get the total number of records in the customers table
+print(session.query(Item).count())  # get the total number of records in the items table
+print(session.query(Order).count())  # get the total number of records in the orders table
 
-session.query(Customer).get(1)
-session.query(Item).get(1)
-session.query(Order).get(100)
+print(session.query(Customer).first())
+print(session.query(Item).first())
+print(session.query(Order).first())
 
-session.query(Customer).filter(Customer.first_name == 'John').all()
+print(session.query(Customer).get(1))
+print(session.query(Item).get(1))
+print(session.query(Order).get(100))
+
+print(session.query(Customer).filter(Customer.first_name == 'John').all())
 
 print(session.query(Customer).filter(Customer.first_name == 'John'))
 
-session.query(Customer).filter(Customer.id <= 5, Customer.town == "Norfolk").all()
+print(session.query(Customer).filter(Customer.id <= 5, Customer.town == "Norfolk").all())
 
-session.query(Customer).filter(or_(
+print(session.query(Customer).filter(or_(
     Customer.town == 'Peterbrugh',
     Customer.town == 'Norfolk'
-)).all()
+)).all())
 
 # find all customers whose first name is John and live in Norfolk
 
-session.query(Customer).filter(and_(
+print(session.query(Customer).filter(and_(
     Customer.first_name == 'John',
     Customer.town == 'Norfolk'
-)).all()
+)).all())
 
 # find all johns who don't live in Peterbrugh
 
-session.query(Customer).filter(and_(
+print(session.query(Customer).filter(and_(
     Customer.first_name == 'John',
     not_(
         Customer.town == 'Peterbrugh',
     )
-)).all()
+)).all())
 
-session.query(Order).filter(Order.date_placed == None).all()
+print(session.query(Order).filter(Order.date_placed == None).all())
 
-session.query(Customer).filter(Customer.first_name.in_(['Toby', 'Sarah'])).all()
+print(session.query(Customer).filter(Customer.first_name.in_(['Toby', 'Sarah'])).all())
 
-session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all()
+print(session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all())
 
-session.query(Item).filter(Item.cost_price.between(10, 50)).all()
+print(session.query(Item).filter(Item.cost_price.between(10, 50)).all())
 
-session.query(Item).filter(not_(Item.cost_price.between(10, 50))).all()
+print(session.query(Item).filter(not_(Item.cost_price.between(10, 50))).all())
 
-session.query(Item).filter(Item.name.like("%r")).all()
+print(session.query(Item).filter(Item.name.like("%r")).all())
 
+print(session.query(Item).filter(Item.name.ilike("w%")).all())
+
+print(session.query(Item).filter(not_(Item.name.like("W%"))).all())
+
+print(session.query(Customer).limit(2).all())
+print(session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all())
+
+print(session.query(Customer).limit(2).offset(2).all())
+
+print(session.query(Customer).limit(2).offset(2))
+
+print(session.query(Item).filter(Item.name.ilike("wa%")).all())
+print(session.query(Item).filter(Item.name.ilike("wa%")).order_by(Item.cost_price).all())
+
+from sqlalchemy import desc
+
+print(session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost_price)).all())
+
+print(session.query(Customer).join(Order).all())
+
+print(session.query(Customer).join(Order))
+
+print(session.query(Customer.id, Customer.username, Order.id).join(Order).all())
+
+print(session.query(Customer).join(Order).join(OrderLine).join(Item).all())
+
+print()
+'''
+session.query(
+    Customer.first_name,
+    Item.name,
+    Item.selling_price,
+    OrderLine.quantity
+).join(Order).join(OrderLine).join(Item).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+    Order.id == 1,
+).all()
+'''
+print(session.query(
+    Customer.first_name,
+    Order.id,
+).outerjoin(Order).all()
+)
+
+
+print(session.query(
+    Customer.first_name,
+    Order.id,
+).outerjoin(Order, full=True).all()
+)
+
+from sqlalchemy import func
+
+print(session.query(func.count(Customer.id)).join(Order).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+).group_by(Customer.id).scalar()
+)
+
+# find the number of customers lives in each town
+
+print(session.query(
+    func.count("*").label('town_count'),
+    Customer.town
+).group_by(Customer.town).having(func.count("*") > 2).all())
+
+print("----------------------------------------------------------------")
+print()
+print("Section 4")
+
+from sqlalchemy import distinct
+
+print(session.query(Customer.town).filter(Customer.id < 10).all())
+print(session.query(Customer.town).filter(Customer.id < 10).distinct().all())
+
+print(session.query(
+    func.count(distinct(Customer.town)),
+    func.count(Customer.town)
+).all())
+
+print("----------------------------------------------------------------")
+print()
+print("Section 5")
+
+from sqlalchemy import cast, Date, distinct, union
+
+print(session.query(
+    cast(func.pi(), Integer),
+    cast(func.pi(), Numeric(10, 2)),
+    cast("2019-12-07", DateTime),
+    cast("2019-12-07", Date),
+).all())
+
+print("----------------------------------------------------------------")
+print()
+print("Section 6")
+
+s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
+s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
+print(s1)
+print(s2)
+print(s1.union(s2).all())
+
+print(s1.union_all(s2).all())
+
+print("----------------------------------------------------------------")
+print()
+print("Section 7")
+
+i = session.query(Item).get(8)
+i.selling_price = 25.91
+print(session.add(i))
+session.commit()
+
+print(session.query(Item).filter(
+    Item.name.ilike("W%")
+).update({"quantity": 60}, synchronize_session='fetch'))
+session.commit()
+
+print("----------------------------------------------------------------")
+print()
+print("Section 8")
+
+i = session.query(Item).filter(Item.name == 'Monitor').one()
+print(i)
+print(session.delete(i))
+session.commit()
+
+print(session.query(Item).filter(
+    Item.name.ilike("W%")
+).delete(synchronize_session='fetch'))
+session.commit()
+
+print("----------------------------------------------------------------")
+print()
+print("Section 9")
+
+from sqlalchemy import text
+
+print(session.query(Customer).filter(text("first_name = 'John'")).all())
+
+print(session.query(Customer).filter(text("town like 'Nor%'")).all())
+
+print(session.query(Customer).filter(text("town like 'Nor%'")).order_by(text("first_name, id desc")).all())
+
+print("----------------------------------------------------------------")
+print()
+print("Section 10")
+
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+
+
+def dispatch_order(order_id):
+    # check whether order_id is valid or not
+    order = session.query(Order).get(order_id)
+
+    if not order:
+        raise ValueError("Invalid order id: {}.".format(order_id))
+
+    if order.date_placed:
+        print("Order already shipped.")
+        return
+
+    try:
+        for i in order.order_lines:
+            i.item.quantity = i.item.quantity - i.quantity
+
+        order.date_placed = datetime.now()
+        session.commit()
+        print("Transaction completed.")
+
+    except IntegrityError as e:
+        print(e)
+        print("Rolling back ...")
+        session.rollback()
+        print("Transaction failed.")
+
+
+dispatch_order(1)
